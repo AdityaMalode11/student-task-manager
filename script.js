4fec706ec4ac0a5132a947e827eb33ac2b693bba
administrator <initial@aditya.com>
2026-08-11 13:49:10 +0530

    Add student task manager web app

---
+++ script.js
@@ -0,0 +1,28 @@
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        <span>${taskText}</span>
+        <button class="delete-btn" onclick="deleteTask(this)">
+            Delete
+        </button>
+    `;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
